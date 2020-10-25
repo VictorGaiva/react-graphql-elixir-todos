a3f26dd37a2731281e1f8decffc6e393e3d04b79
Victor Gaiva <victor.gaiva@redetendencia.com.br>
2020-10-25 16:39:18 -0400

    API setup and data structures

---
+++ lib/todo_list_web/api/schema.ex
@@ -0,0 +1,53 @@
+defmodule TodoListWeb.Api.Schema do
+  use Absinthe.Schema
+  alias TodoList.Todos
+
+  object :todo_item do
+    field :id, non_null(:id)
+    field :content, non_null(:string)
+
+    field :is_completed, non_null(:boolean) do
+      resolve(fn %{completed_at: completed_at}, _, _ ->
+        {:ok, !is_nil(completed_at)}
+      end)
+    end
+  end
+
+  mutation do
+    field :create_todo_item, non_null(:boolean) do
+      arg(:content, non_null(:string))
+
+      resolve(fn %{content: content}, _ ->
+        case Todos.create_item(%{content: content}) do
+          {:ok, %Todos.Item{}} ->
+            {:ok, true}
+
+          _ ->
+            {:ok, false}
+        end
+      end)
+    end
+
+    field :toggle_todo_item, :todo_item do
+      arg(:id, non_null(:id))
+
+      resolve(fn %{id: item_id}, _ ->
+        Todos.toggle_by_id(item_id)
+      end)
+    end
+  end
+
+  query do
+    field :hello, :string do
+      resolve(fn _, _ ->
+        {:ok, "Hello World!"}
+      end)
+    end
+
+    field :todo_items, non_null(list_of(:todo_item)) do
+      resolve(fn _, _ ->
+        {:ok, Todos.list_items()}
+      end)
+    end
+  end
+end
---
+++ lib/todo_list_web/router.ex
@@ -0,0 +1,45 @@
+defmodule TodoListWeb.Router do
+  use TodoListWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", TodoListWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  scope "/api" do
+    pipe_through :api
+
+    get "/", Absinthe.Plug.GraphiQL, schema: TodoListWeb.Api.Schema, interface: :playground
+    post "/", Absinthe.Plug, schema: TodoListWeb.Api.Schema
+  end
+
+  # Enables LiveDashboard only for development
+  #
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through :browser
+      live_dashboard "/dashboard", metrics: TodoListWeb.Telemetry
+    end
+  end
+end
